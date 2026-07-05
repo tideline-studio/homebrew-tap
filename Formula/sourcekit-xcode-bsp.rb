621bc981dec1662beb7940439ae48035bc509c2f
Shawn Shi <shawnshi75@gmail.com>
2026-07-05 17:26:23 -0400

    Fix SHA256 to match public tarball

--- Formula/sourcekit-xcode-bsp.rb
+++ Formula/sourcekit-xcode-bsp.rb
@@ -2,7 +2,7 @@ class SourcekitXcodeBsp < Formula
   desc "Build Server Protocol server for Xcode projects"
   homepage "https://github.com/tideline-studio/sourcekit-xcode-bsp"
   url "https://github.com/tideline-studio/sourcekit-xcode-bsp/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "a432975f050608e6e2f58ab550fd1693cde1dca832a28a89cbd18b0c5beea19d"
+  sha256 "008b6d87f99304bcae56219697977a3c7543a2dd02ed27fc09b56ded1ff9179b"
   license "Apache-2.0"
   head "https://github.com/tideline-studio/sourcekit-xcode-bsp.git", branch: "main"
 
